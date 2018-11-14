1fa70e6e8b17994495de7e16025758a8e00232d2
Jiajie <33970456+Jiajie-Mai@users.noreply.github.com>
2018-11-14 06:39:57 -0500

    fixed things

--- Work/24_rest/24_rest.py
+++ Work/24_rest/24_rest.py
@@ -2,18 +2,17 @@
 #SoftDev1 pd7
 #K24 - A RESTful Journey Skyward
 #2018-11-13
-from flask import Flask, render_template  #imports class Flask
-import urllib.request
-import json
-app = Flask(__name__)#Creates an instance of Flask
+from flask import Flask, render_template, request
+import urllib, json
 
-@app.route('/')#Defines index
-def hello_world():
-    f =  urllib.request.urlopen('https://api.nasa.gov/planetary/apod?api_key=KZA7h2LSwdHWgjPm5eQSFcO5QhFBJcWnGe8fJYoj')
-    d = f.read()#Reads f and stores Json inside d
-    data = json.loads(d)
-    return render_template("24_rest.html", pic = data['url'], explanation = data['explanation'])
+app = Flask(__name__)
 
-if (__name__) == "__main__":#if this file is run directly then the Flask app will run
-    app.debug = True#allows changes to directly affect local host without rerunning app
+@app.route('/')
+def index():
+    output = urllib.request.urlopen('https://api.nasa.gov/planetary/apod?api_key=XZ5YhAvHZNp90JaM7AUZORCL27mkB8V8x7U2YEny')
+    string = output.read()
+    dictonary = json.loads(string)
+    return render_template('24_rest.html',picture=dictonary['url'],caption=dictonary['explanation'],date=dictonary['date'])
+if (__name__) == "__main__":
+    app.debug = True
     app.run()
